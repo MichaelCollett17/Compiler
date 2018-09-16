c4c0313220f1be17c3b70aa623d1081520289ee4
Michael Collett <michaelcollett17@gmail.com>
2018-09-15 21:54:18 -0500

    working freaking lexical analyzer :) hell yeah... just need to insert all my listing file prints and print my token file all pretty at the end... then Ill design my two files. Then I do a writeup gang gang gang

--- Machines/Number.c
+++ Machines/Number.c
@@ -4,18 +4,30 @@
 #include <ctype.h>
 #include "machines.h"
 
-int checkError(int state, int xx, int yy, int zz, char lead1, char trail2, char lead3){
-  return 0;
-}
+struct numsOut
+{
+  struct machOut out;
+  int xx;
+  int yy;
+  int zz;
+  char lead1;
+  char trail2;
+  char lead3;
+  int state;
+};
 
-struct machOut numbers(int b, int end, char *buff, FILE *listFile){
+// int checkError(int state, int xx, int yy, int zz, char lead1, char trail2, char lead3){
+//   return 0;
+// }
+
+struct numsOut numbers(int b, int end, char *buff, FILE *listFile){
   int state = 0;//1 int, 2 sreal, 3 lreal
   int xx = 0;
   int yy = 0;
   int zz = 0;
   char lead1 = 'a';
-  char lead2 = 'b';
-  char lead3 = 'c';
+  char trail2 = 'a';
+  char lead3 = 'a';
   int f=b;
 
   if(isdigit(buff[f])!=0){
@@ -23,7 +35,8 @@ struct machOut numbers(int b, int end, char *buff, FILE *listFile){
     while(isdigit(buff[f])!=0){
       f++;
     }
-    xx=f;
+    xx=f-b;
+    lead1 = buff[b];
     // if(((f-b)>1) && buff[b]=='0'){//CHECK LEADING ZERO
     //   //LEADING ZERO
     //   char *lexeme = malloc((f-b+1)*sizeof(char));
@@ -40,10 +53,12 @@ struct machOut numbers(int b, int end, char *buff, FILE *listFile){
         while(isdigit(buff[f])!=0){
           f++;
         }
-        yy = f-1;//minus 1 for period
+        yy = f-b-xx-1;//minus 1 for period
+        trail2 = buff[f-1];
         if(buff[f] == 'E'){//took out option for e because it isn't listed among pascals lexical conventions
           f++;
           if(isdigit(buff[f])!=0){
+            lead3=buff[f];
             f++;
             // if(buff[f-1] == '0'){
             //   //EXPLEADZERO
@@ -59,19 +74,21 @@ struct machOut numbers(int b, int end, char *buff, FILE *listFile){
             while(isdigit(buff[f])!=0){
               f++;
             }
-            zz = f - 2;//period, e
+            zz = f-b-yy-xx-1-1;//period, e,xx,yy
             //return lreal d.dEd
-            state = 2;//sreal
+            state = 3;//lreal
             char *lexeme = malloc((f-b+1)*sizeof(char));
             const char* from = buff;
             strncpy(lexeme, from+b, f-b);
             lexeme[f-b] = '\0';
             struct machOut out = {f, 0, lexeme, LREAL, 0};
-            return out;
+            struct numsOut numOut = {out, xx, yy, zz,lead1, trail2, lead3, state};
+            return numOut;
           }
           else if(buff[f] == '+' || buff[f] == '-'){
             f++;
             if(isdigit(buff[f])!=0){
+              lead3=buff[f];
               f++;
               // if(buff[f-1] == '0'){
               //   //EXPLEADZERO
@@ -87,17 +104,20 @@ struct machOut numbers(int b, int end, char *buff, FILE *listFile){
               while(isdigit(buff[f])!=0){
                 f++;
               }
-              zz = f - 3;//period, e, +/-
+              zz = f-b-yy-xx-1-2;//period, e, +/-, xx, yy
+              state = 3;
               //return lreal
               char *lexeme = malloc((f-b+1)*sizeof(char));
               const char* from = buff;
               strncpy(lexeme, from+b, f-b);
               lexeme[f-b] = '\0';
               struct machOut out = {f, 0, lexeme, LREAL, 0};
-              return out;
+              struct numsOut numOut = {out, xx, yy, zz,lead1, trail2, lead3, state};
+              return numOut;
             }
             else{
               f--;
+              //trail2 = buff[f-1];
               //d.dE+O
               // if(buff[f-1] == '0'){
               //   //DECTRAILZERO
@@ -114,10 +134,12 @@ struct machOut numbers(int b, int end, char *buff, FILE *listFile){
               strncpy(lexeme, from+b, f-b);
               lexeme[f-b] = '\0';
               struct machOut out = {f, 0, lexeme, SREAL, 0};
-              return out;
+              struct numsOut numOut = {out, xx, yy, zz,lead1, trail2, lead3, state};
+              return numOut;
             }
           }
           else{
+            //trail2 = buff[f-1];
             // if(buff[f-1] == '0'){
             //   //DECTRAILZERO
             //   char *lexeme = malloc((f-b+1)*sizeof(char));
@@ -133,7 +155,8 @@ struct machOut numbers(int b, int end, char *buff, FILE *listFile){
             strncpy(lexeme, from+b, f-b);
             lexeme[f-b] = '\0';
             struct machOut out = {f, 0, lexeme, SREAL, 0};
-            return out;
+            struct numsOut numOut = {out, xx, yy, zz,lead1, trail2, lead3, state};
+            return numOut;
           }
         }
         else{
@@ -152,7 +175,8 @@ struct machOut numbers(int b, int end, char *buff, FILE *listFile){
           strncpy(lexeme, from+b, f-b);
           lexeme[f-b] = '\0';
           struct machOut out = {f, 0, lexeme, SREAL, 0};
-          return out;
+          struct numsOut numOut = {out, xx, yy, zz,lead1, trail2, lead3, state};
+          return numOut;
         }
       }
       else{ //d.O
@@ -163,7 +187,8 @@ struct machOut numbers(int b, int end, char *buff, FILE *listFile){
         strncpy(lexeme, from+b, f-b);
         lexeme[f-b] = '\0';
         struct machOut out = {f, 0, lexeme, INT, 0};
-        return out;
+        struct numsOut numOut = {out, xx, yy, zz,lead1, trail2, lead3, state};
+        return numOut;
       }
     }
     else{
@@ -173,15 +198,97 @@ struct machOut numbers(int b, int end, char *buff, FILE *listFile){
       strncpy(lexeme, from+b, f-b);
       lexeme[f-b] = '\0';
       struct machOut out = {f, 0, lexeme, INT, 0};
-      return out;
+      struct numsOut numOut = {out, xx, yy, zz,lead1, trail2, lead3, state};
+      return numOut;
     }
   }
   else{
     struct machOut block = {BLOCK, -1, "\0", -1, -1};
-    return block;
+    struct numsOut blockOut = {block};
+    return blockOut;
   }
 }
 
 struct machOut number(int b, int end, char *buff, FILE *listFile){
-  return numbers(b,end,buff,listFile);
+  struct numsOut numOut = numbers(b,end,buff,listFile);
+  //check for all errors!
+  if(numOut.state == 1){//int
+    if(numOut.xx > 10){
+      struct machOut out = numOut.out;
+      struct machOut err = {out.b, INTTOOLONG, out.lexeme};
+      return err;
+    }
+    else if(numOut.lead1 == '0'){
+      struct machOut out = numOut.out;
+      struct machOut err = {out.b, LEADZERO, out.lexeme};
+      return err;
+    }
+    else{
+      return numOut.out;
+    }
+  }
+  else if(numOut.state == 2){//SREAL
+    if(numOut.xx > 5){
+      struct machOut out = numOut.out;
+      struct machOut err = {out.b, SREAL1TOOLONG, out.lexeme};
+      return err;
+    }
+    else if(numOut.yy > 5){
+      struct machOut out = numOut.out;
+      struct machOut err = {out.b, SREAL2TOOLONG, out.lexeme};
+      return err;
+    }
+    else if(numOut.trail2 == '0'){
+      struct machOut out = numOut.out;
+      struct machOut err = {out.b, DECTRAILZERO, out.lexeme};
+      return err;
+    }
+    else if(numOut.lead1 == '0'){
+      struct machOut out = numOut.out;
+      struct machOut err = {out.b, LEADZERO, out.lexeme};
+      return err;
+    }
+    else{
+      return numOut.out;
+    }
+  }
+  else if(numOut.state == 3){
+    printf("zz: %d\n", numOut.zz);
+    if(numOut.xx > 5){
+      struct machOut out = numOut.out;
+      struct machOut err = {out.b, LREAL1TOOLONG, out.lexeme};
+      return err;
+    }
+    else if(numOut.yy > 5){
+      struct machOut out = numOut.out;
+      struct machOut err = {out.b, LREAL2TOOLONG, out.lexeme};
+      return err;
+    }
+    else if(numOut.zz > 2){
+      struct machOut out = numOut.out;
+      struct machOut err = {out.b, LREAL3TOOLONG, out.lexeme};
+      return err;
+    }
+    else if(numOut.trail2 == '0'){
+      struct machOut out = numOut.out;
+      struct machOut err = {out.b, DECTRAILZERO, out.lexeme};
+      return err;
+    }
+    else if(numOut.lead1 == '0'){
+      struct machOut out = numOut.out;
+      struct machOut err = {out.b, LEADZERO, out.lexeme};
+      return err;
+    }
+    else if(numOut.lead3 == '0'){
+      struct machOut out = numOut.out;
+      struct machOut err = {out.b, EXPLEADZERO, out.lexeme};
+      return err;
+    }
+    else{
+      return numOut.out;
+    }
+  }
+  else{
+    return numOut.out;
+  }
 }
